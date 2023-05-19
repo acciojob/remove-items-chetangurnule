6f695fdf1f489636d3d93e9902c637305eabea4e
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 12:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let button = document.querySelector("#btn");
+button.addEventListener("click", ()=>{
+    let selectList = document.querySelector("#colorSelect");
+    let options = document.querySelectorAll("option");
+    options.forEach((option)=>{
+        if(selectList.value == option.textContent){
+            option.remove();
+        }
+    })
+});
