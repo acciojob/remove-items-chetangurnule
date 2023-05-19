1788a30343cf4eb13152466dd4a843dc074ac11b
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 15:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //your JS code here. If required.
 let button = document.querySelector("#btn");
-let option = document.querySelectorAll("option");
-button.addEventListener("click", ()=>{
-    let selectList = document.querySelector("#colorSelect");
-    option.forEach((element)=>{
-        if(selectList.value == element.textContent){
-            element.remove();
-        }
-    })
-});
+let div = document.querySelector("#colorSelect");
+button.addEventListener("click", removeElement)
+
+function removeElement(){
+    div.remove(div.selectedIndex);
+ };
