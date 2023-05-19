9e3b1adc9e6659d30d86a3c1cb2413268bb4e48f
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 13:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 let button = document.querySelector("#btn");
+let option = document.querySelectorAll("option");
 button.addEventListener("click", ()=>{
     let selectList = document.querySelector("#colorSelect");
-    let option = document.querySelectorAll("option");
     option.forEach((element)=>{
         if(selectList.value == element.textContent){
             element.remove();
